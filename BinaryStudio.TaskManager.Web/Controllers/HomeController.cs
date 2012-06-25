14b109993a1bc0ee2edd4107a259b47ddbd75556
Nikita Exaurov <exaurov.nm@mail.ru>
2012-06-26 00:19:57 +0300

    Added Views for all managers and for single
    Models Task and Manager need refactoring.

--- BinaryStudio.TaskManager.Web/Controllers/HomeController.cs
+++ BinaryStudio.TaskManager.Web/Controllers/HomeController.cs
@@ -1,23 +1,43 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BinaryAcademia.AllManagerView.Models;
+using BinaryStudio.TaskManager.Web.Content;
 
-namespace BinaryStudio.TaskManager.Web.Controllers
+
+namespace BinaryAcademia.AllManagerView.Controllers
 {
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Welcome to ASP.NET MVC!";
-
+            AllManagersDataTest allManagersDataTest = new AllManagersDataTest();
+            ViewBag.Managers = allManagersDataTest.GetManagers();
             return View();
         }
 
         public ActionResult About()
         {
             return View();
+        }
+        [HttpGet]
+        public ActionResult AllManagersWithTasks()
+        {
+
+            //Create some customize managers and tasks for testing AllManagerWithTasks View
+            AllManagersDataTest testManagersData = new AllManagersDataTest();
+            return View(testManagersData.GetManagers());
+        }
+        [HttpGet]
+        public ActionResult ManagersTask(int id)
+        {
+            AllManagersDataTest allManagersDataTest = new AllManagersDataTest();
+            ICollection<ManagerModel> managers = allManagersDataTest.GetManagers();
+            return View(managers.Single(x => x.ManagerId == id).Tasks);
         }
     }
 }
---
+++ BinaryStudio.TaskManager.Web/Models/TaskModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BinaryAcademia.AllManagerView.Models;
+
+namespace BinaryStudio.TaskManager.Web.Models
+{
+    //Must be fixed 
+    public class TaskModel
+    {
+        public int TaskId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public virtual ManagerModel Manager { get; set; }
+
+        public TaskModel(
+            int id,
+            string title,
+            string description)
+        {
+            TaskId = id;
+            Title = title;
+            Description = description;
+        }
+
+        public TaskModel(
+            int id,
+            string title,
+            string description,
+            ManagerModel manager)
+        {
+            TaskId = id;
+            Title = title;
+            Description = description;
+            Manager = manager;
+        }
+
+
+    }
+}
