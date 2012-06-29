5957b4b719464e49383cea0fccedc46f13bdefe8
Nikita Exaurov <exaurov.nm@mail.ru>
2012-06-29 15:25:05 +0300

    Fixed builds problems with file TaskViewModel.cs

---
+++ BinaryStudio.TaskManager.Web/Models/TaskViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BinaryStudio.TaskManager.Logic.Domain;
+
+namespace BinaryStudio.TaskManager.Web.Models
+{
+    public class TaskViewModel
+    {
+        public HumanTask Task { get; set; }
+        public string CreatorName { get; set; }
+    }
+}
