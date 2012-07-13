8d4469458cce2486800e268ecd53a75eb4d63685
ShulipaNik <nshulipa@gmail.com>
2012-07-13 15:25:12 +0300

    deleting views with entity Employee

--- BinaryStudio.TaskManager.Web/Controllers/AdminController.cs
+++ BinaryStudio.TaskManager.Web/Controllers/AdminController.cs
@@ -1,37 +1,30 @@
 namespace BinaryStudio.TaskManager.Web.Controllers
 {
-    using System;
-    using System.Linq;
     using System.Web.Mvc;
-    using System.Web.Security;
-
     using BinaryStudio.TaskManager.Logic.Core;
     using BinaryStudio.TaskManager.Logic.Domain;
     using BinaryStudio.TaskManager.Web.Models;
 
+    [Authorize(Roles = "admin")]
     public class AdminController : Controller
     {
         private readonly IUserRepository userRepository;
-                
+
         public AdminController(IUserRepository userRepository)
         {
-            this.userRepository = userRepository;            
+            this.userRepository = userRepository;
         }
 
-        [Authorize(Roles = "admin")]
         public ActionResult Index()
         {
             return this.View();
         }
 
-
-        [Authorize(Roles = "admin")]
         public ActionResult RegisterNewUser()
         {
             return this.View();
         }
 
-        [Authorize(Roles = "admin")]
         [HttpPost]
         public ActionResult RegisterNewUser(RegisterNewUserModel model)
         {
@@ -55,14 +48,12 @@ namespace BinaryStudio.TaskManager.Web.Controllers
             // If we got this far, something failed, redisplay form
             return this.View(model);
         }
-  
-        [Authorize(Roles = "admin")]
+
         public ActionResult UsersList()
         {
             return this.View(this.userRepository.GetAll());
         }
 
-        [Authorize(Roles = "admin")]
         public ActionResult EditUser(int id)
         {
             User user = this.userRepository.GetById(id);
@@ -70,7 +61,6 @@ namespace BinaryStudio.TaskManager.Web.Controllers
         }
 
         [HttpPost]
-        [Authorize(Roles = "admin")]
         public ActionResult EditUser(User user)
         {
             if (this.ModelState.IsValid)
@@ -81,28 +71,25 @@ namespace BinaryStudio.TaskManager.Web.Controllers
             return this.View(user);
         }
 
-        [Authorize(Roles = "admin")]
         public ActionResult DetailsUser(int id)
         {
-            User user= this.userRepository.GetById(id);
+            User user = this.userRepository.GetById(id);
             return this.View(user);
         }
 
-        [Authorize(Roles = "admin")]
         public ActionResult DeleteUser(int id)
         {
             User user = this.userRepository.GetById(id);
             return this.View(user);
-        }        
+        }
 
         /// <summary>
         /// Deleting account and moving all task to unassigned.
         /// </summary>
-        /// <param name="id">The id.</param>
+        /// <param name="id">The user id.</param>
         /// <returns></returns>
         [HttpPost]
         [ActionName("DeleteUser")]
-        [Authorize(Roles = "admin")]
         public ActionResult DeleteUserConfirmed(int id)
         {
             this.userRepository.Delete(id);
@@ -110,7 +97,6 @@ namespace BinaryStudio.TaskManager.Web.Controllers
         }
 
         [HttpPost]
-        [Authorize(Roles = "admin")]
         public ActionResult SendUserInfo(int id)
         {
             return this.Json(userRepository.GetById(id));
