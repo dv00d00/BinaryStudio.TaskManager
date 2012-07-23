44b8f1e26fca391c304f35d805abc8ad87528b1f
ShulipaNik <nshulipa@gmail.com>
2012-07-23 21:33:43 +0300

    Implements test ShouldNot_LogonUser_WhenSuchUserExistButNotVerify

--- BinaryStudio.TaskManager.Logic.Tests/UserProcessorTests.cs
+++ BinaryStudio.TaskManager.Logic.Tests/UserProcessorTests.cs
@@ -72,7 +72,22 @@ namespace BinaryStudio.TaskManager.Logic.Tests
         }      
   
         [Test]
-        public void ShouldNot_LogonUser_WhenSuchUserDoesNotExist()
+        public void ShouldNot_LogonUser_WhenSuchUserDoesNotExistAndVerify()
+        {
+            this.cryptoProvider = new CryptoProvider();
+            const string Username = "username";
+            const string Password = "password";
+            this.userRepositoryMock.Setup(x => x.GetByName(Username)).Returns((User)null);
+
+            // act
+            var result = this.userProcessor.LogOnUser(Username, Password);
+
+            // arrange
+            Assert.AreEqual(result, false);
+        }
+
+        [Test]
+        public void ShouldNot_LogonUser_WhenSuchUserExistButNotVerify()
         {
             this.cryptoProvider = new CryptoProvider();
             const string Username = "username";
@@ -87,13 +102,13 @@ namespace BinaryStudio.TaskManager.Logic.Tests
                 UserName = Username,
                 Email = Email,
                 Credentials = new Credentials
-                    {
-                        Salt = salt,
-                        Passwordhash = this.cryptoProvider.CreateCryptoPassword(Password, salt),
-                        IsVerify = true
-                    }
+                {
+                    Salt = salt,
+                    Passwordhash = this.cryptoProvider.CreateCryptoPassword(Password, salt),
+                    IsVerify = false
+                }
             };
-            this.userRepositoryMock.Setup(x => x.GetByName(Username)).Returns((User)null);
+            this.userRepositoryMock.Setup(x => x.GetByName(Username)).Returns(user);
 
             // act
             var result = this.userProcessor.LogOnUser(Username, Password);
