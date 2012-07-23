288e94ed8f494ad5d3d32caf5342b8397d2d54d7
ExaurovNM <exaurov@gmail.com>
2012-07-23 22:27:59 +0300

    SignalR

---
+++ BinaryStudio.TaskManager.Web/Controllers/SinalRController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BinaryStudio.TaskManager.Web.Controllers
+{
+    public class SinalRController : Controller
+    {
+        //
+        // GET: /SinalR/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ BinaryStudio.TaskManager.Web/SignalR/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using SignalR.Hubs;
+
+namespace BinaryStudio.TaskManager.Web.SignalR
+{
+    public class ChatHub : Hub
+    {
+        public void Distribute(string message)
+        {
+            Clients.Receive(message);
+        }
+    }
+}
