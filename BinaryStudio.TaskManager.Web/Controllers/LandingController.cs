fa35ced21527eb4e1a7de080e205fa6b7b3dc753
nikeiwe <nikeiwe@mail.ru>
2012-07-24 15:45:38 +0300

    connected DI to LandingController, began to implement Add method

--- BinaryStudio.TaskManager.Web/Controllers/LandingController.cs
+++ BinaryStudio.TaskManager.Web/Controllers/LandingController.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 
 namespace BinaryStudio.TaskManager.Web.Controllers
 {
+    using BinaryStudio.TaskManager.Logic.Core;
     using BinaryStudio.TaskManager.Logic.Domain;
     using BinaryStudio.TaskManager.Web.Models;
 
@@ -13,18 +14,42 @@ namespace BinaryStudio.TaskManager.Web.Controllers
     {
         //
         // GET: /Landing/
+        private IProjectRepository projectRepository;
+
+        private IUserRepository userRepository;
+
+        public LandingController(IProjectRepository projectRepository, IUserRepository userRepository)
+        {
+            this.projectRepository = projectRepository;
+            this.userRepository = userRepository;
+        }
 
         public ActionResult Index()
         {
             var model = new LandingModel();
-                model.Projects = new List<Project>{
-                new Project { Id = 1, Name = "1asdfsdf", Description = "1deasdasdasdas" },
-                new Project { Id = 2,  Name = "2asdfsdf", Description = "2deasdasdasdas" },
-                new Project { Id = 3,  Name = "3asdfsdf", Description = "3deasdasdasdas" },
-                new Project { Id = 4,  Name = "4asdfsdf", Description = "4deasdasdasdas" }
-            };
+            model.Projects = new List<Project>
+                {
+                    new Project { Id = 1, Name = "1asdfsdf", Description = "1deasdasdasdas" },
+                    new Project { Id = 2, Name = "2asdfsdf", Description = "2deasdasdasdas" },
+                    new Project { Id = 3, Name = "3asdfsdf", Description = "3deasdasdasdas" },
+                    new Project { Id = 4, Name = "4asdfsdf", Description = "4deasdasdasdas" }
+                };
             return this.View(model);
         }
 
-    }
+        [HttpPost]
+        private ActionResult AddProject(string projectName)
+        {
+            var project = new Project
+                {
+                    Created = DateTime.Now,
+                    Creator = this.userRepository.GetByName(@User.Identity.Name),
+                    Name = projectName                    
+                };
+            this.projectRepository.Add(project);
+            var model = new LandingModel();
+            return this.Json(model);
+        }
+
+}
 }
