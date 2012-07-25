adb1fff32aea0fbf690e2038e355c81cd1dd525e
ShulipaNik <nshulipa@gmail.com>
2012-07-25 18:41:55 +0300

    (Issue #105) - domain model ready to invitation

---
+++ BinaryStudio.TaskManager.Storage/Domain/Invitation.cs
@@ -0,0 +1,20 @@
+﻿namespace BinaryStudio.TaskManager.Logic.Domain
+{
+    public class Invitation : IEntity
+    {
+        public int Id { get; set; }
+
+        public int ProjectId { get; set; }
+
+        public  Project Project { get; set; }
+
+        public int UserId { get; set; }
+
+        public  User User { get; set; }
+
+        public bool IsInvitationSended { get; set; }
+
+        public bool IsInvitationConfirmed { get; set; }
+
+    }
+}
