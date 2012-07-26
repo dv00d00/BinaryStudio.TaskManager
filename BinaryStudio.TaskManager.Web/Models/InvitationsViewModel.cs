c2923bdd1e8e1485dc4f997a724b895b0b876af1
ShulipaNik <nshulipa@gmail.com>
2012-07-26 05:59:39 +0300

    #108 - first implementation of InvitationsViewModel

---
+++ BinaryStudio.TaskManager.Web/Models/InvitationsViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace BinaryStudio.TaskManager.Web.Models
+{
+    using BinaryStudio.TaskManager.Logic.Domain;
+
+    public class InvitationsViewModel
+    {
+        public User User { get; set; }
+
+        public Project Project { get; set; }
+
+        public Invitation Invitation { get; set; }
+    }
+}
