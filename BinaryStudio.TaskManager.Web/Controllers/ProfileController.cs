84a9644631c7ec6df95a20672c60810e4851a44e
Sova1989 <ann-sov@yandex.ru>
2012-07-27 19:11:13 +0300

    additional commit for ProfileController.cs and ProfileModel.cs

---
+++ BinaryStudio.TaskManager.Web/Controllers/ProfileController.cs
@@ -0,0 +1,97 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ProfileController.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   Defines the ProfileController type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace BinaryStudio.TaskManager.Web.Controllers
+{
+    using System.Web;
+    using System.Web.Mvc;
+
+    using BinaryStudio.TaskManager.Logic.Core;
+    using BinaryStudio.TaskManager.Logic.Domain;
+    using BinaryStudio.TaskManager.Web.Models;
+
+    /// <summary>
+    /// The profile controller.
+    /// </summary>
+    public class ProfileController : Controller
+    {
+        /// <summary>
+        /// The user processor.
+        /// </summary>
+        private readonly IUserProcessor userProcessor;
+
+        /// <summary>
+        /// The user repository.
+        /// </summary>
+        private readonly IUserRepository userRepository;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ProfileController"/> class.
+        /// </summary>
+        /// <param name="userProcessor">
+        /// The user processor.
+        /// </param>
+        /// <param name="userRepository">
+        /// The user repository.
+        /// </param>
+        public ProfileController(IUserProcessor userProcessor, IUserRepository userRepository)
+        {
+            this.userProcessor = userProcessor;
+            this.userRepository = userRepository;
+        }
+
+        /// <summary>
+        /// The get image in profile.
+        /// </summary>
+        /// <param name="userName">
+        /// The user name.
+        /// </param>
+        /// <returns>
+        /// The System.Web.Mvc.ActionResult.
+        /// </returns>
+        [Authorize]
+        public ActionResult GetImageInProfile(string userName)
+        {
+            User user = this.userProcessor.GetUserByName(userName);
+            if (user != null)
+            {
+                return this.File(user.ImageData, user.ImageMimeType);
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// The profile changes.
+        /// </summary>
+        /// <param name="model">
+        /// The model.
+        /// </param>
+        /// <param name="image">
+        /// The image.
+        /// </param>
+        /// <returns>
+        /// The System.Web.Mvc.ActionResult.
+        /// </returns>
+        [HttpPost]
+        public ActionResult ProfileChanges(ProfileModel model, HttpPostedFileBase image)
+        {
+            if (ModelState.IsValid)
+            {
+                if (image != null)
+                {
+                    model.ImageMimeType = image.ContentType;
+                    model.ImageData = new byte[image.ContentLength];
+                    image.InputStream.Read(model.ImageData, 0, image.ContentLength);
+                }
+            }
+            return this.RedirectToAction("Profile", "Account");
+        }
+    }
+}
