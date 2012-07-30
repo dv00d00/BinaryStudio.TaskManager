c156de741aa3055d04e4d12eee6f3abd0d5f2037
ShulipaNik <nshulipa@gmail.com>
2012-07-30 15:51:33 +0300

    #114 - CRUD for entity user, Project

--- BinaryStudio.TaskManager.Web/Controllers/AdminController.cs
+++ BinaryStudio.TaskManager.Web/Controllers/AdminController.cs
@@ -4,26 +4,57 @@ namespace BinaryStudio.TaskManager.Web.Controllers
     using BinaryStudio.TaskManager.Logic.Core;
     using BinaryStudio.TaskManager.Logic.Domain;
 
-    using System.Linq;
-
+    /// <summary>
+    /// The admin controller for managing application data.
+    /// </summary>
     [Authorize(Roles = "admin")]
     public class AdminController : Controller
     {
+        /// <summary>
+        /// The user repository.
+        /// </summary>
         private readonly IUserRepository userRepository;
 
+        /// <summary>
+        /// The project repository.
+        /// </summary>
         private readonly IProjectRepository projectRepository;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AdminController"/> class.
+        /// </summary>
+        /// <param name="userRepository">
+        /// The user repository.
+        /// </param>
+        /// <param name="projectRepository">
+        /// The project repository.
+        /// </param>
         public AdminController(IUserRepository userRepository, IProjectRepository projectRepository)
         {
             this.userRepository = userRepository;
             this.projectRepository = projectRepository;
         }
 
+        /// <summary>
+        /// The users list.
+        /// </summary>
+        /// <returns>
+        /// The System.Web.Mvc.ActionResult.
+        /// </returns>
         public ActionResult UsersList()
         {
             return this.View(this.userRepository.GetAll());
         }
 
+        /// <summary>
+        /// The edit user.
+        /// </summary>
+        /// <param name="userId">
+        /// The user id.
+        /// </param>
+        /// <returns>
+        /// The System.Web.Mvc.ActionResult.
+        /// </returns>
         public ActionResult EditUser(int userId)
         {
             var user = this.userRepository.GetById(userId);
@@ -94,27 +125,57 @@ namespace BinaryStudio.TaskManager.Web.Controllers
         [HttpPost]
         [ActionName("DeleteUser")]
         public ActionResult DeleteUserConfirmed(int userId)
-        {                   
-            this.userRepository.DeleteUser(userId);            
+        {
+            this.userRepository.DeleteUser(userId);
             return this.RedirectToAction("AdminPanel");
         }
 
+        /// <summary>
+        /// The admin panel.
+        /// </summary>
+        /// <returns>
+        /// The System.Web.Mvc.ActionResult.
+        /// </returns>
         public ActionResult AdminPanel()
         {
             return this.View();
         }
 
+        /// <summary>
+        /// The projects list.
+        /// </summary>
+        /// <returns>
+        /// The System.Web.Mvc.ActionResult.
+        /// </returns>
         public ActionResult ProjectsList()
         {
             return this.View(this.projectRepository.GetAll());
         }
 
+        /// <summary>
+        /// The edit project.
+        /// </summary>
+        /// <param name="projectId">
+        /// The project id.
+        /// </param>
+        /// <returns>
+        /// The System.Web.Mvc.ActionResult.
+        /// </returns>
         public ActionResult EditProject(int projectId)
         {
             var project = this.projectRepository.GetById(projectId);
             return this.View(project);
         }
 
+        /// <summary>
+        /// The edit project.
+        /// </summary>
+        /// <param name="project">
+        /// The project.
+        /// </param>
+        /// <returns>
+        /// The System.Web.Mvc.ActionResult.
+        /// </returns>
         [HttpPost]
         public ActionResult EditProject(Project project)
         {
@@ -124,6 +185,53 @@ namespace BinaryStudio.TaskManager.Web.Controllers
                 return this.RedirectToAction("AdminPanel");
             }
             return this.View(project);
+        }
+
+        /// <summary>
+        /// The details project.
+        /// </summary>
+        /// <param name="projectId">
+        /// The project id.
+        /// </param>
+        /// <returns>
+        /// The System.Web.Mvc.ActionResult.
+        /// </returns>
+        public ActionResult DetailsProject(int projectId)
+        {
+            var project = this.projectRepository.GetById(projectId);
+            return this.View(project);
+        }
+
+        /// <summary>
+        /// The delete project.
+        /// </summary>
+        /// <param name="projectId">
+        /// The project id.
+        /// </param>
+        /// <returns>
+        /// The System.Web.Mvc.ActionResult.
+        /// </returns>
+        public ActionResult DeleteProject(int projectId)
+        {
+            var project = this.projectRepository.GetById(projectId);
+            return this.View(project);
+        }
+
+        /// <summary>
+        /// The delete project confirmed.
+        /// </summary>
+        /// <param name="projectId">
+        /// The project id.
+        /// </param>
+        /// <returns>
+        /// The System.Web.Mvc.ActionResult.
+        /// </returns>
+        [HttpPost]
+        [ActionName("DeleteProject")]
+        public ActionResult DeleteProjectConfirmed(int projectId)
+        {
+            this.projectRepository.Delete(projectId);
+            return this.RedirectToAction("AdminPanel");
         }
     }
 }
