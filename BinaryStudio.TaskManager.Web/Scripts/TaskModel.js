27878424aca8e5445c460d1543cd717ea1afb8d6
nikeiwe <nikeiwe@mail.ru>
2012-07-31 12:11:05 +0300

    started working with knockout

---
+++ BinaryStudio.TaskManager.Web/Scripts/TaskModel.js
@@ -0,0 +1,11 @@
+﻿var TaskModel = function (tasks) {
+    var self = this;
+    self.contacts = ko.observableArray(ko.utils.arrayMap(tasks, function (task) {
+        return {
+            id: task.Id,
+            name: task.Name,
+            description: task.Description,
+            priority: task.Priority
+        };
+    }));
+}
