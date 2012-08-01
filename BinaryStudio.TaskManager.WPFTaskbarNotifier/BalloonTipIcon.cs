4e03285eaa2bc9c548c4d5efe9940327782c23f3
ExaurovNM <exaurov.nm@gmail.com>
2012-08-02 01:47:35 +0300

    Add Notifier popup projects

---
+++ BinaryStudio.TaskManager.WPFTaskbarNotifier/BalloonTipIcon.cs
@@ -0,0 +1,14 @@
+
+/// Used with permission from Mariano Omar Rodriguez
+/// http://weblogs.asp.net/marianor/archive/2007/10/15/a-wpf-wrapper-around-windows-form-notifyicon.aspx
+
+namespace WPFTaskbarNotifier
+{
+	public enum BalloonTipIcon
+	{
+		None = 0,
+		Info = 1,
+		Warning = 2,
+		Error = 3,
+	}
+}
