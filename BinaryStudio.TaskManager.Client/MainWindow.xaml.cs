a5f641dfe2509930e3800c7879a527eac2bff9bc
ExaurovNM <exaurov.nm@gmail.com>
2012-08-01 20:03:55 +0300

    Visual changes in Client

--- BinaryStudio.TaskManager.Client/MainWindow.xaml.cs
+++ BinaryStudio.TaskManager.Client/MainWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿namespace MessengR.Client
+﻿using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using Microsoft.Practices.Prism.Commands;
+
+namespace MessengR.Client
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -8,6 +12,10 @@
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Header_Loaded(object sender, System.Windows.RoutedEventArgs e)
+        {
         }
     }
 }
