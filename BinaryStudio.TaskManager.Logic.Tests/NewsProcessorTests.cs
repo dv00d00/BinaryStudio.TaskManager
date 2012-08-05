13d940634f1d788b7e43d7a4a6dd9e33e09e2663
ExaurovNM <exaurov.nm@gmail.com>
2012-08-05 21:00:07 +0300

    #152 More NewsProcessor Tests

--- BinaryStudio.TaskManager.Logic.Tests/NewsProcessorTests.cs
+++ BinaryStudio.TaskManager.Logic.Tests/NewsProcessorTests.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using BinaryStudio.TaskManager.Logic.Core;
 using BinaryStudio.TaskManager.Logic.Domain;
 using Moq;
+using Moq.Language.Flow;
 using NUnit.Framework;
 
 namespace BinaryStudio.TaskManager.Logic.Tests
@@ -46,7 +48,7 @@ namespace BinaryStudio.TaskManager.Logic.Tests
         }
 
         [Test]
-        public void Should_AddNews()
+        public void Should_AddNews_WithNewsAsParametr()
         {
             // arrange
             News news = new News
@@ -60,6 +62,60 @@ namespace BinaryStudio.TaskManager.Logic.Tests
 
             // assert
             this.newsRepositoryMock.Verify(mock => mock.AddNews(news), Times.AtLeastOnce());
+        }
+
+        [Test]
+        public void Should_AddCorrectNews_WithHumanTaskHistoryAsParametr()
+        {
+            // arrange 
+            User user = new User
+                            {
+                                UserName = "UserName",
+                                Id = 2
+                            };
+            HumanTaskHistory taskHistory = new HumanTaskHistory
+                                               {
+                                                   Id = 1,
+                                                   Action = "Create",
+                                               };
+            News news = new News
+                            {
+                                HumanTaskHistory = taskHistory,
+                                HumanTaskHistoryId = taskHistory.Id,
+                                Id = 1,
+                                IsRead = false,
+                                User = user,
+                                UserId = user.Id
+                            };
+            // act
+
+            this.newsProcessor.AddNews(taskHistory, user);
+
+            // assert
+            this.newsRepositoryMock.Verify(mock => mock.AddNews(It.Is<News>(x => 
+                x.HumanTaskHistoryId == taskHistory.Id
+                && x.UserId == user.Id
+                && x.User == user
+                && x.HumanTaskHistory == taskHistory)), 
+                Times.Once());
+        }
+
+        [Test]
+        public void Should_CreateNewsForUsers()
+        {
+            // arrange
+            User creator = new User{UserName = "Creator", Id = 1};
+            User collaborator = new User { UserName = "Collaborator", Id = 2 };
+            this.projectProcessorMock.Setup(it => it.GetUsersAndCreatorInProject(1)).Returns(value: new List<User> {creator, collaborator});
+            HumanTaskHistory taskHistory = new HumanTaskHistory
+                                               {
+                                                   Id = 1,
+                                                   Action = "Create"
+                                               };
+            // act
+            this.newsProcessor.CreateNewsForUsersInProject(taskHistory,1);
+            // assert
+            this.newsRepositoryMock.Verify(it => it.AddNews(It.IsAny<News>()), Times.Exactly(2));
         }
     }
 }
