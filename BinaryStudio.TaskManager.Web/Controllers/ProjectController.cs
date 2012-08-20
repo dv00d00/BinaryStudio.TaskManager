5e53930caa8f038ad4441d05dea64c0389042f92
Sova1989 <ann-sov@yandex.ru>
2012-08-20 12:23:51 +0300

    delete task popup dialog

--- BinaryStudio.TaskManager.Web/Controllers/ProjectController.cs
+++ BinaryStudio.TaskManager.Web/Controllers/ProjectController.cs
@@ -583,18 +583,9 @@ namespace BinaryStudio.TaskManager.Web.Controllers
         /// The System.Web.Mvc.ActionResult.
         /// </returns>
         [HttpPost]
-        //[ActionName("Delete")]
-        public ActionResult DeleteConfirmed(int idTask, int projectId, bool? viewStyle)
+        public void DeleteConfirmed(int idTask, int projectId, bool? viewStyle)
         {
-            //var userId = this.taskProcessor.GetTaskById(idTask).AssigneeId;
             this.taskProcessor.DeleteTask(idTask);
-            
-            if (true == viewStyle)
-            {
-                //return this.RedirectToAction("MultiuserView", new { projectId, userId });
-            }
-
-            return this.RedirectToAction("Project", new { id = projectId });
         }
         
         /// <summary>
